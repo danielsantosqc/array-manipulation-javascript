298e728d9730c2f2de386f95fc9e040fc3f57aa2
Daniel Santos <danielsantosqc.dev@gmail.com>
2022-02-12 08:33:47 -0600

    update

---
+++ server/03-map-reolated.js
@@ -0,0 +1,21 @@
+const orders = [
+  {
+    customerName: "Nicolas",
+    total: 60,
+    delivered: true,
+  },
+  {
+    customerName: "Zulema",
+    total: 120,
+    delivered: false,
+  },
+  {
+    customerName: "Santiago",
+    total: 180,
+    delivered: true,
+  },
+  {
+    customerName: "Valentina",
+    total: 240,
+    delivered: true,
+  }, 
