dca88bc07b4224f17a34cef05cd3018518f3b715
Daniel Santos <danielsantosqc.dev@gmail.com>
2022-02-17 02:59:50 -0600

    update 04-filter.js

--- server/04-filter.js
+++ server/04-filter.js
@@ -1,6 +1,47 @@
 const words = ['spray', 'limit', 'elite', 'exuberant'];
 
-const newArray = words.filter(item => item.length < 6 );
+// with for
+// const newArray = [];
+// for (let index = 0; index < words.length; index++) {
+//     const item = words[index];
+//     if (item.length >= 6) {
+//         newArray.push(item);
+//     }
+// }
+// console.log('original', words);
+// console.log('newArray', newArray);
 
+// with filter method
+// filter create new array
+const newArray = words.filter(item => item.length < 6 );
 console.log('original', words);
 console.log('newArray', newArray);
+
+
+// -------------------------------------------
+const orders = [
+  {
+    customerName: "Nicolas",
+    total: 60,
+    delivered: true,
+  },
+  {
+    customerName: "Zulema",
+    total: 120,
+    delivered: false,
+  },
+  {
+    customerName: "Santiago",
+    total: 180,
+    delivered: true,
+  },
+  {
+    customerName: "Valentina",
+    total: 240,
+    delivered: true,
+  },
+];
+
+const newArray2 = orders.filter(item => item.delivered && item.total > 100);
+console.log('original',orders);
+console.log('newArray' ,newArray2);
