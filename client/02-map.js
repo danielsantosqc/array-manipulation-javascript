4429c6e4a7b17bde1d5592be0f2333ab42b57572
Daniel Santos <danielsantosqc.dev@gmail.com>
2022-02-17 02:24:41 -0600

    update

--- client/02-map.js
+++ client/02-map.js
@@ -2,6 +2,12 @@ let lista = document.getElementById("lista");
 
 const cositas = ['👻','👽','😎','🦖'];
 
+// const newArray = [];
+// for (let index = 0; index < letters.length; index++) {
+//     const element = letters[index];
+//     newArray.push(element + '++');
+// }
+
 const newArray = cositas.map(item => {
     return  `<li>${item}</li>`;
   }
