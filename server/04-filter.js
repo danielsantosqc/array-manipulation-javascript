8edd172f43b0da2189af4ccb81bf3ee8f936cf78
Daniel Santos <danielsantosqc.dev@gmail.com>
2022-02-17 02:26:27 -0600

    add file  04-filter.js

---
+++ server/04-filter.js
@@ -0,0 +1,6 @@
+const words = ['spray', 'limit', 'elite', 'exuberant'];
+
+const newArray = words.filter(item => item.length < 6 );
+
+console.log('original', words);
+console.log('newArray', newArray);
