a800946485a1a419c1ccd36ddf86fa4cf075c887
Daniel Santos <danielsantosqc.dev@gmail.com>
2022-02-21 20:15:40 -0600

    update 05-reduce.js with folder server

--- server/05-reduce.js
+++ server/05-reduce.js
@@ -1,4 +1,4 @@
-const numbers = [1,2,3,4,5];
+const items = [1,2,3,4,5];
 
-const result = numbers.reduce((accumulator, element) => accumulator + element, 0);
+const result = items.reduce((accumulator, item) => accumulator + item, 0);
 console.log('respuesta', result);
