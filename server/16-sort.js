2cbd2341e60953a2b34ab74859dce23df41ff461
Daniel Santos <danielsantosqc.dev@gmail.com>
2022-02-26 23:11:38 -0600

    add 16-sort.js in server folder

---
+++ server/16-sort.js
@@ -0,0 +1,37 @@
+const months = ['March', 'Jan', 'Feb', 'Dec'];
+months.sort();
+console.log(months);
+
+const numbers = [1, 30, 4, 21, 100000];
+numbers.sort((a,b) => b - a);
+console.log(numbers);
+
+const words = ['réservé', 'premier', 'communiqué', 'café', 'adieu', 'éclair', 'banana'];
+// words.sort();
+words.sort((a,b) => a.localeCompare(b));
+console.log(words);
+
+const orders = [
+    {
+      customerName: "Nicolas",
+      total: 600,
+      delivered: true,
+    },
+    {
+      customerName: "Zulema",
+      total: 120,
+      delivered: false,
+    },
+    {
+      customerName: "Santiago",
+      total: 1840,
+      delivered: true,
+    },
+    {
+      customerName: "Valentina",
+      total: 240,
+      delivered: true,
+    },
+  ];
+  orders.sort((a,b) => a.total - b.total);
+  console.log(orders);
